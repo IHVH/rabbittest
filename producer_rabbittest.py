6d94f9e261892d244caa3db00e928d2025ad01bb
IHVH <IHVH@inbox.ru>
2022-11-12 13:31:16 +0300

    while producer

--- producer_rabbittest.py
+++ producer_rabbittest.py
@@ -1,17 +1,22 @@
 import pika
-
+count = 9999
+msg = 'Hello World!'
 connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.111', credentials= pika.PlainCredentials('test', 'test') ))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+while count > 0:
+    channel.basic_publish(exchange='', routing_key='hello', body=f'{msg} - {count}')
+    print(f" [x] Sent '{msg} {count}'")
+    channel.basic_publish(exchange='syslog', routing_key='qwer', body=f'hello2 {msg} - {count}')
+    print(f" [x] Sent 'hello2 {msg} {count}'")
+    count = count - 1
 
-channel.basic_publish(exchange='syslog',
-                      routing_key='qwer',
-                      body='log Hello World!')
-print(" [x] Sent 'Hello World!'")
+fin = "finished"
+channel.basic_publish(exchange='', routing_key='hello', body=fin)
+print(f" [x] Sent '{fin}'")
+
+channel.basic_publish(exchange='syslog', routing_key='qwer', body='hello2 ' + fin)
+print(f" [x] Sent 'hello2 {fin}'")
 
 connection.close()
