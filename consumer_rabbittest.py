eac2597973ebfa26915ad976dba052b96b83b173
IHVH <IHVH@inbox.ru>
2023-04-29 14:18:28 +0300

    class 20230429

--- consumer_rabbittest.py
+++ consumer_rabbittest.py
@@ -1,18 +1,30 @@
 import pika, sys, os
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.111', credentials= pika.PlainCredentials('test', 'test') ))
+    host_name = '192.168.1.111'
+    cred = pika.PlainCredentials('test', 'test')
+    parametrs = pika.ConnectionParameters(host_name, credentials=cred )
+
+    connection = pika.BlockingConnection(parametrs)
     channel = connection.channel()
     channel2 = connection.channel()
+    channel3 = connection.channel()
 
+    args = {"x-max-length": 1000}
     channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='test1000', arguments=args)
     channel2.queue_declare(queue='hello2', durable=True)
+    #channel3.queue_declare(queue='testExclusive', exclusive=True, arguments=args)
+    channel3.queue_declare(queue='testAutoDelete', auto_delete=True, arguments=args)
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
+    channel.basic_consume(queue='test1000', on_message_callback=callback, auto_ack=True)
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
     channel2.basic_consume(queue='hello2', on_message_callback=callback, auto_ack=True)
+    #channel3.basic_consume(queue='testExclusive', on_message_callback=callback, auto_ack=True)
+    channel3.basic_consume(queue='testAutoDelete', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- producer_rabbittest.py
+++ producer_rabbittest.py
@@ -1,22 +1,38 @@
 import pika
 count = 9999
 msg = 'Hello World!'
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.111', credentials= pika.PlainCredentials('test', 'test') ))
+
+host_name = '192.168.1.111'
+cred = pika.PlainCredentials('test', 'test')
+parametrs = pika.ConnectionParameters(host_name, credentials=cred )
+
+connection = pika.BlockingConnection(parametrs)
 channel = connection.channel()
+args = {"x-max-length": 1000}
+channel.queue_declare(queue='test1000', arguments=args)
 channel.queue_declare(queue='hello')
+channel.queue_declare(queue='testExclusive', exclusive=True, arguments=args)
+channel.queue_declare(queue='testAutoDelete', auto_delete=True, arguments=args)
+
+channel.basic_publish(exchange='', routing_key='testExclusive', body=f'testExclusive {msg} - {count}')
+print(f" [x] Sent 'testExclusive {msg} {count}'")
 
 while count > 0:
     channel.basic_publish(exchange='', routing_key='hello', body=f'{msg} - {count}')
     print(f" [x] Sent '{msg} {count}'")
-    channel.basic_publish(exchange='syslog', routing_key='qwer', body=f'hello2 {msg} - {count}')
+    channel.basic_publish(exchange='', routing_key='test1000', body=f'test1000 {msg} - {count}')
+    print(f" [x] Sent 'test1000 {msg} {count}'")
+    channel.basic_publish(exchange='syslog', routing_key='rout1', body=f'hello2 {msg} - {count}')
     print(f" [x] Sent 'hello2 {msg} {count}'")
+    channel.basic_publish(exchange='', routing_key='testAutoDelete', body=f'testAutoDelete {msg} - {count}')
+    print(f" [x] Sent 'testAutoDelete {msg} {count}'")
     count = count - 1
 
 fin = "finished"
 channel.basic_publish(exchange='', routing_key='hello', body=fin)
 print(f" [x] Sent '{fin}'")
 
-channel.basic_publish(exchange='syslog', routing_key='qwer', body='hello2 ' + fin)
+channel.basic_publish(exchange='syslog', routing_key='rout1', body='hello2 ' + fin)
 print(f" [x] Sent 'hello2 {fin}'")
 
 connection.close()
